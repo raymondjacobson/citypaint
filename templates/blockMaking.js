c2c33b4937294ca70d3e3bd14d1a327e601dee73
Maureen Daum <daummaureen@gmail.com>
2014-09-06 23:11:26 -0400

    make a block in svg

--- templates/blockMaking.js
+++ templates/blockMaking.js
@@ -1,3 +1,8 @@
-function makeBlock(x, y) {
-	
+function makeBlock(x, y, length) {
+	d3.select("#area")
+		.append("rect")
+		.attr("x", x)
+		.attr("y", y)
+		.attr("width", length)
+		.attr("height", length)
 }
